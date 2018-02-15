827e3106321ac30f366a7cfdb059b6e1a5c37778
Wesley S Whiteree <whitereews@egr-maclab-26.local>
2018-02-15 14:48:43 -0500

    updated

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,4 +1,7 @@
+import java.util.*;
 public class ArrayBasedStack<T> implements StackInterface<T>{
+
+
 
 private int topOfStack;
 private int INITIAL_CAPACITY;
@@ -6,36 +9,59 @@ private boolean initialized;
 private T[] data;
 
 
-public ArrayBasedStack<T>{
-
+ ArrayBasedStack(int capacity){
+ if(capacity <= 0)
+ {
+ throw new IllegalArgumentException("Cannot create array less than 0");
+ }
+ else{
+ 
+ T[] array = (T[]) new Object[capacity];
 topOfStack = 0;
 INITIAL_CAPACITY = 5;
-intitialized = true;
-
+initialized = true;
+}
 }
 
 public void push(T newEntry){
+   if(topOfStack == data.length)
+   throw new IllegalArgumentException(" ...");
    
+   topOfStack++;
+   data[topOfStack] = newEntry;
    }
   
    /** Removes and returns this stack's top entry.
        @return  The object at the top of the stack. 
        @throws  EmptyStackException if the stack is empty before the operation. */
    public T pop(){
-   return null;
+   
+   
+   if(data.length == 0)
+   throw new IllegalArgumentException("Array is empty");
+   
+   T temp = data[topOfStack - 1];
+   data[topOfStack - 1] = null;
+   topOfStack--;
+   return temp;
    }
   
    /** Retrieves this stack's top entry.
        @return  The object at the top of the stack.
        @throws  EmptyStackException if the stack is empty. */
    public T peek(){
-   return null;
+   if(data.length == 0)
+   throw new IllegalArgumentException("Array is empty");
+   return data[topOfStack];
    }
   
    /** Detects whether this stack is empty.
        @return  True if the stack is empty. */
    public boolean isEmpty(){
+   if(data.length == 0)
    return true;
+   else
+   return false;
    }
   
    /** Removes all entries from this stack. */
