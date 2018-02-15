5fa3bcdb06f8a18b3a649a3cfec1f218d9b60017
Wesley S Whiteree <whitereews@egr-maclab-26.local>
2018-02-15 14:11:49 -0500

    Implemented array based stack

---
+++ ArrayBasedStack.java
@@ -0,0 +1,50 @@
+public class ArrayBasedStack<T> implements StackInterface<T>{
+
+private int topOfStack;
+private int INITIAL_CAPACITY;
+private boolean initialized;
+private T[] data;
+
+
+public ArrayBasedStack<T>{
+
+topOfStack = 0;
+INITIAL_CAPACITY = 5;
+intitialized = true;
+
+}
+
+public void push(T newEntry){
+   
+   }
+  
+   /** Removes and returns this stack's top entry.
+       @return  The object at the top of the stack. 
+       @throws  EmptyStackException if the stack is empty before the operation. */
+   public T pop(){
+   return null;
+   }
+  
+   /** Retrieves this stack's top entry.
+       @return  The object at the top of the stack.
+       @throws  EmptyStackException if the stack is empty. */
+   public T peek(){
+   return null;
+   }
+  
+   /** Detects whether this stack is empty.
+       @return  True if the stack is empty. */
+   public boolean isEmpty(){
+   return true;
+   }
+  
+   /** Removes all entries from this stack. */
+   public void clear(){
+   
+   }
+
+
+
+
+
+}
